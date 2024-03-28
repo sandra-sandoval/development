1bc443e0a2bcafbed82d49d5e831f888eb1726d8
Sandra Sandoval <sandra_sandoval@brown.edu>
2024-03-27 21:34:19 -0400

    added styling

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from "react";
 import CourseCard from "./components/course"; // Importing CourseCard component
 import courseData from "./assets/courseData.json";
+import "./App.css";
 
 const CourseSearchApp = () => {
   const [courses, setCourses] = useState(courseData);
+  // const [updated, setUpdate] = useState([]);
   const [original, setOriginal] = useState(courseData);
   const [favorites, setFavorites] = useState([]);
   const [departmentFilter, setDepartmentFilter] = useState("All");
@@ -35,14 +37,12 @@ const CourseSearchApp = () => {
     const favoritedCourses = updatedCourses.filter(
       (course) => course.favorite === true
     );
-
     setFavorites(favoritedCourses);
   };
 
   const handleDepartmentFilter = (e) => {
     const selectedDept = e.target.value;
     setDepartmentFilter(selectedDept);
-    // setCourses(filterDept);
     if (selectedDept === "All") {
       setCourses(original);
     } else {
@@ -65,9 +65,93 @@ const CourseSearchApp = () => {
       setCourses(filterWrit);
     }
   };
+  // const mergeCourses = () => {
+  //   const allCourses = [...courses, ...favorites];
+  //   // console.log("courses"   ...courses );
+  //   console.log({ ...favorites });
+
+  //   return allCourses;
+  // };
+  const clearFavorites = () => {
+    // const favoriteCourses = courses.filter((course) => course.favorite);
+    // setCourses(courses, ...favoriteCourses);
+    // setCourses((courses) => [...courses, "hello"]);
+    // let updated = [];
+    // console.log(courses);
+    // const update = courses.filter((course) => course.favorite === true);
+    // updated.push(...update);
+    // updated.push(...courses);
+    // console.log(updated);
+    // setCourses(updated);
+    setCourses(original);
+    setFavorites([]);
+  };
+  const resetFilters = () => {
+    setCourses(original);
+    setDepartmentFilter("All");
+    setWrit("All");
+  };
+  // const handleFilter = (e, category) => {
+  //   if (category === "dept") {
+  //     const selectedDept =
+  //       departmentFilter === "All" ? e.target.value : departmentFilter;
+  //     setDepartmentFilter(selectedDept);
+  //   }
+  //   if (category === "writ") {
+  //     const selectedWrit = writ === "All" ? e.target.value : writ;
+  //     setWrit(selectedWrit);
+  //   }
+
+  //   // console.log({ selectedDept });
+  //   // console.log({ selectedWrit });
+
+  //   let filteredCourses = original;
+
+  //   if (departmentFilter !== "All") {
+  //     filteredCourses = filteredCourses.filter(
+  //       (course) => course.Department === departmentFilter
+  //     );
+  //   }
+
+  //   if (writ !== "All") {
+  //     filteredCourses = filteredCourses.filter(
+  //       (course) => course.WRIT === (writ === "True")
+  //     );
+  //   }
+
+  //   setCourses(filteredCourses);
+  // };
+
+  // useEffect(() => {
+  //   handleFilter();
+  // }, [departmentFilter, writ]);
+
   return (
     <div>
-      <h1>Course Search</h1>
+      <h1>Course Manager</h1>
+      <div id="filters-container">
+        <div className="filter">
+          <h2>Filter By Department</h2>
+          <select value={departmentFilter} onChange={handleDepartmentFilter}>
+            <option value={"All"}>All</option>
+            <option value={"CSCI"}>CSCI</option>
+            <option value={"BIOL"}>BIOL</option>
+            <option value={"CHEM"}>CHEM</option>
+            <option value={"HIST"}>HIST</option>
+            <option value={"APMA"}>APMA</option>
+            <option value={"HISP"}>HISP</option>
+          </select>
+        </div>
+        <div className="filter">
+          <h2>Filter by Writ</h2>
+          <select value={writ} onChange={handleWritFilter}>
+            <option value={"All"}>All</option>
+            <option value={"True"}>True</option>
+            <option value={"False"}>False</option>
+          </select>
+        </div>
+        <button onClick={resetFilters}>Reset Filters</button>
+      </div>
       <div className="course-list">
         {/* filtering out the favorited courses from the course list display */}
         {courses
@@ -86,7 +170,7 @@ const CourseSearchApp = () => {
             onFavoriteToggle={toggleFavoriteCourse}
           />
         ))} */}
-        <div className="filters">
+        {/* <div className="filters">
           <h2>Filter By Department</h2>
           <select value={departmentFilter} onChange={handleDepartmentFilter}>
             <option value={"All"}>All</option>
@@ -106,6 +190,7 @@ const CourseSearchApp = () => {
             <option value={"False"}>False</option>
           </select>
         </div>
+        <button onClick={resetFilters}>Reset Filters</button> */}
       </div>
       <div>
         <h2>Favorited Courses</h2>
@@ -118,6 +203,7 @@ const CourseSearchApp = () => {
             />
           ))}
         </div>
+        <button onClick={clearFavorites}>Clear Favorites</button>
       </div>
     </div>
   );
--- src/components/course.jsx
+++ src/components/course.jsx
@@ -1,19 +1,50 @@
 import React from "react";
+import "./course.css";
+// import "/Users/sandrasandoval/Desktop/cs1300/development/src/components/course.css";
 
 const CourseCard = ({ course, onFavoriteToggle }) => {
   const { Title, Department, num, WRIT, Rating, favorite } = course;
 
   return (
-    <div className="course-card">
-      <h2>{Title}</h2>
-      {/* <p>Professor: {professor}</p> */}
-      <p>Department: {Department}</p>
-      <p>Course #: {num}</p>
-      <p> WRIT : {WRIT}</p>
-      <p>Rating: {Rating}</p>
-      <button onClick={() => onFavoriteToggle(course.id)}>
-        {favorite ? "Remove from Favorites" : "Add to Favorites"}
-      </button>
+    <div className="course">
+      <div className="course-card">
+        <h2>{Title}</h2>
+        {/* <p>Professor: {professor}</p> */}
+        <p>
+          <span span style={{ fontWeight: "bold" }}>
+            {" "}
+            Department:
+          </span>{" "}
+          {Department}
+        </p>
+        <p>
+          <span span style={{ fontWeight: "bold" }}>
+            {" "}
+            Course # :
+          </span>{" "}
+          {num}
+        </p>
+        <p>
+          {" "}
+          <span span style={{ fontWeight: "bold" }}>
+            {" "}
+            WRIT:
+          </span>{" "}
+          {WRIT}
+        </p>
+        <p>
+          <span span style={{ fontWeight: "bold" }}>
+            {" "}
+            Rating:
+          </span>{" "}
+          {Rating}
+        </p>
+      </div>
+      <div className="favorite-button">
+        <button onClick={() => onFavoriteToggle(course.id)}>
+          {favorite ? "Remove" : "Favorite"}
+        </button>
+      </div>
     </div>
   );
 };
